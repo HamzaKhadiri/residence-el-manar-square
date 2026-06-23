048381748a8f083bacbc6751803e33b91fa8733d
HamzaKhadiri <hamz.khadiri@gmail.com>
2026-06-23 21:44:19 +0100

    app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown("""
 @st.cache_resource
 def get_gspread_client():
     scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-  creds_dict = dict(st.secrets["gcp_service_account"])
+    creds_dict = dict(st.secrets["gcp_service_account"])
 
 # إعداد الصلاحيات
 scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
