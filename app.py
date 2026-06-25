59f6da7d69a21b04dca2f7a34676a2e330e52acd
HamzaKhadiri <hamz.khadiri@gmail.com>
2026-06-25 23:07:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -448,7 +448,7 @@ if is_admin:
     # --- الجزء الخاص بالساكن ---
 else:
 
-           st.write("### 📋 وضعية اشتراكاتك الخاصة:")
+            st.write("### 📋 وضعية اشتراكاتك الخاصة:")
             user_name = st.session_state.get("user_name", "")
             df_user_cotis = df[
         df["Nom et prénom / الاسم الكامل"]
