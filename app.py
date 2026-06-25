08ce2ba3f35459377cce9015c5dbdce247c48a64
HamzaKhadiri <hamz.khadiri@gmail.com>
2026-06-25 22:01:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -451,9 +451,9 @@ if is_admin:
 else:
 
            st.write("### 📋 وضعية اشتراكاتك الخاصة:")
- user_name = st.session_state.get("user_name", "")
+user_name = st.session_state.get("user_name", "")
 
- df_user_cotis = df[
+df_user_cotis = df[
         df["Nom et prénom / الاسم الكامل"]
         .str.contains(user_name, case=False, na=False)
     ].copy()
