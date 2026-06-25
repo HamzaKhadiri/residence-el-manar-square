3546127f0a2bbe2dd43ee76e711d227a70370a50
HamzaKhadiri <hamz.khadiri@gmail.com>
2026-06-25 22:26:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -448,7 +448,7 @@ elif st.session_state.current_page == "Cotisations":
                 st.error(f"Erreur : {e}")
 
     # --- الجزء الخاص بالساكن ---
-   else:
+    else:
 
            st.write("### 📋 وضعية اشتراكاتك الخاصة:")
             user_name = st.session_state.get("user_name", "")
