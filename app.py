71650cc232480137f2d72239029b94f63755c724
HamzaKhadiri <hamz.khadiri@gmail.com>
2026-06-25 21:55:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -295,7 +295,7 @@ elif st.session_state.current_page == "Cotisations":
         return val
 
     # --- الجزء الخاص بالأدمن ---
-if is_admin:
+    if is_admin:
 
     row1, row2 = st.columns(2), st.columns(2)
 
