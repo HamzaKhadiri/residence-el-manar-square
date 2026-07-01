70668cb6f96f3310c5d1c134461f068cdf8d7978
HamzaKhadiri <hamz.khadiri@gmail.com>
2026-07-01 18:30:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -640,9 +640,9 @@ with c4:
     st.metric("🟡 Surveillance", nb_surveillance)
 
 st.divider() 
-                st.subheader(f"⚠️ الوضعية التراكمية حتى {selected_month_t2} {selected_year_t2}")
-                st.dataframe(top_debtors, use_container_width=True, hide_index=True)
-            else:
-                st.success("🎉 لا توجد متأخرات في هذه الفترة.")
+st.subheader(f"⚠️ الوضعية التراكمية حتى {selected_month_t2} {selected_year_t2}")
+st.dataframe(top_debtors, use_container_width=True, hide_index=True)
+ else:
+     st.success("🎉 لا توجد متأخرات في هذه الفترة.")
 
 
