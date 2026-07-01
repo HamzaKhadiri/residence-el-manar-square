d22f4d24cbe2f67b5f985bc086753594bae8f56f
HamzaKhadiri <hamz.khadiri@gmail.com>
2026-07-01 19:51:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -620,6 +620,17 @@ elif st.session_state.current_page == "Rapports":
                 top_debtors["Niveau"] = top_debtors["Nb_Retards"].apply(get_alert_color)
                 
                 st.subheader(f"⚠️ الوضعية التراكمية حتى {selected_month_t2} {selected_year_t2}")
-                st.dataframe(top_debtors, use_container_width=True, hide_index=True)
+
+if not top_debtors.empty:
+    # نقوم بحساب الارتفاع بناءً على عدد الصفوف + مساحة للرأس (Header)
+    # 35 هو ارتفاع الصف الواحد تقريباً، نضيف 50 بكسل لرأس الجدول
+    dynamic_height = (len(top_debtors) * 35) + 50
+    
+    st.dataframe(
+        top_debtors, 
+        use_container_width=True, 
+        hide_index=True,
+        height=dynamic_height # هنا ستظهر كل الصفوف مهما كان عددها
+    )
             else:
                 st.success("🎉 لا توجد متأخرات في هذه الفترة.")
